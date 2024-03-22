ff923254ef589d8df2081c54e8d5f57ed3617ef6
Shashini Nilukshi <123292001+ShashiniNilukshi@users.noreply.github.com>
2024-03-22 17:38:39 +0530

    Footer Update

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,11 +1,3 @@
-import React from 'react'
-
-function Footer() {
-  return (
-    <footer className='styles.footer'>
-      <p>&copy; 2024 Shashini Nilukshi. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
+version https://git-lfs.github.com/spec/v1
+oid sha256:55f29b368b7ea41baba4174a53b5591489fc88a82a21836b2ba9e5f80e66ae67
+size 237
