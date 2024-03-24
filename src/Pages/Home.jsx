d3c30dfd44b27eb3ea50f6617ef40a265c9d5eb0
Shashini Nilukshi <123292001+ShashiniNilukshi@users.noreply.github.com>
2024-03-24 14:27:18 +0530

    Fixed errors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5bf253856246094816f77ca5da18f2f9924e9b3beb0d23bbd5d061960c8741d
-size 2624
+oid sha256:9a63ac532772370e440c61aac55d0f369942ce7af4872f835efed6e8cd367ae4
+size 2626
